b0221089b123f4d3772fa6cd80f08066cb07efa8
mok <scutmox@foxmail.com>
2018-08-13 21:57:17 +0800

    del login helper vue

--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
 import Vue from 'vue'
 import App from './App'
 import store from './store'
-import LoginHelper from './components/login-helper'
 
 Vue.prototype.$store=store;
-Vue.prototype.$LoginHelper=LoginHelper;
 
 Vue.config.productionTip = false
 App.mpType = 'app'
